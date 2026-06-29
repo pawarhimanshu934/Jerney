ec79a54851449513338f7b8f2c743ef7fc2f4a36
Ubuntu <ubuntu@ip-172-31-4-232.ec2.internal>
2026-06-29 21:52:40 +0000

    terraform code added

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,3 +1,4 @@
+#EKS Managed Node Group Mode (live)
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
@@ -5,77 +6,148 @@ module "eks" {
   name               = var.cluster_name
   kubernetes_version = var.cluster_version
 
+  addons = {
+    coredns = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      before_compute = true
+    }
+  }
+
+  # Optional: Additional security group rules to add to the cluster security group. This is useful for allowing access to the cluster from other security groups, such as a bastion host or CI/CD system.
+  node_security_group_additional_rules = {
+    ingress_self_all = {
+      description = "Node to node all ports/protocols"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      self        = true
+    }
+  }
+
   # Optional
   endpoint_public_access = true
 
   # Optional: Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
 
-  compute_config = {
-    enabled    = true
-    node_pools = ["general-purpose", "system"]
-  }
-
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.private_subnets
 
-  # Auth mode required for Auto Mode, Controls how access to the cluster is managed.
-  authentication_mode = "API"
+  # EKS Managed Node Group(s)
+  eks_managed_node_groups = {
+    jerney-node-group = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.medium"]
 
-  #Encrypts Kubernetes Secrets using AWS KMS, it encrypt things like DB passwords API keys, Tokens and store encrypted in etcd
-  encryption_config = {
-    resorces = ["secrets"]
-  }
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
 
-  #Sends control plane logs to CloudWatch
-  enabled_log_types = [
-    "api",
-    "audit",
-    "authenticator",
-    "controllerManager",
-    "scheduler"
-  ]
+      capacity_type = "SPOT"
+      disk_size     = 30
 
+      labels = {
+        "node-group" = "jersey"
+      }
+    }
 
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
+    tags = {
+      Environment = "dev"
+      Terraform   = "true"
+    }
   }
 }
 
-
-# | Setup                           | Behavior                                     |
-# | ------------------------------- | -------------------------------------------- |
-# | `["general-purpose"]`           | All workloads share same nodes               |
-# | `["general-purpose", "system"]` | System workloads isolated on dedicated nodes,  |
-
-# 🟢 general-purpose :
-# Default pool
-# Runs:
-#   Your applications
-#   Deployments
-#   Services
-#   No strict isolation
-
-# 🔵 system : 
-# Dedicated pool for Kubernetes system components
-# Runs things like:
-#   CoreDNS
-#   kube-proxy
-#   VPC CNI
-#   AWS controllers (ALB controller, etc.)
+#keeping worker and controller plane in same private subnets for now, but can be changed to have worker nodes in public subnets if needed.
+#using API Gateway to access the cluster from outside the VPC, so no need for public subnets for worker nodes.
 
 
-# Log	Purpose
-# api	-> All API requests
-# audit ->	Who did what (security critical)
-# authenticator ->	IAM auth activity
-# controllerManager ->	Internal controllers
-# scheduler	-> Pod scheduling decisions
+#EKS Auto Mode (Preview)
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "~> 21.0"
 
-# Why it matters
+#   name               = var.cluster_name
+#   kubernetes_version = var.cluster_version
 
-# ✔ Debugging issues
-# ✔ Security auditing
-# ✔ Observability
+#   # Optional
+#   endpoint_public_access = true
+
+#   # Optional: Adds the current caller identity as an administrator via cluster access entry
+#   enable_cluster_creator_admin_permissions = true
+
+#   compute_config = {
+#     enabled    = true
+#     node_pools = ["general-purpose", "system"]
+#   }
+
+#   vpc_id                   = module.vpc.vpc_id
+#   subnet_ids               = module.vpc.private_subnets
+#   control_plane_subnet_ids = module.vpc.private_subnets
+
+#   # Auth mode required for Auto Mode, Controls how access to the cluster is managed.
+#   authentication_mode = "API"
+
+#   #Encrypts Kubernetes Secrets using AWS KMS, it encrypt things like DB passwords API keys, Tokens and store encrypted in etcd
+#   encryption_config = {
+#     resorces = ["secrets"]
+#   }
+
+#   #Sends control plane logs to CloudWatch
+#   enabled_log_types = [
+#     "api",
+#     "audit",
+#     "authenticator",
+#     "controllerManager",
+#     "scheduler"
+#   ]
+
+
+#   tags = {
+#     Environment = "dev"
+#     Terraform   = "true"
+#   }
+# }
+
+
+# # | Setup                           | Behavior                                     |
+# # | ------------------------------- | -------------------------------------------- |
+# # | `["general-purpose"]`           | All workloads share same nodes               |
+# # | `["general-purpose", "system"]` | System workloads isolated on dedicated nodes,  |
+
+# # 🟢 general-purpose :
+# # Default pool
+# # Runs:
+# #   Your applications
+# #   Deployments
+# #   Services
+# #   No strict isolation
+
+# # 🔵 system : 
+# # Dedicated pool for Kubernetes system components
+# # Runs things like:
+# #   CoreDNS
+# #   kube-proxy
+# #   VPC CNI
+# #   AWS controllers (ALB controller, etc.)
+
+
+# # Log	Purpose
+# # api	-> All API requests
+# # audit ->	Who did what (security critical)
+# # authenticator ->	IAM auth activity
+# # controllerManager ->	Internal controllers
+# # scheduler	-> Pod scheduling decisions
+
+# # Why it matters
+
+# # ✔ Debugging issues
+# # ✔ Security auditing
+# # ✔ Observability
---
+++ terraform/readme.md
@@ -0,0 +1,8 @@
+AWS EKS Arch using Terraform
+
+Resource Created :
+VPC
+EKS
+
+Why VPC is needed?
+-> To secure your EKS cluster, using a VPC is best practice. 
